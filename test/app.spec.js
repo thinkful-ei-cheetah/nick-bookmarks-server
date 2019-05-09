7219942e3f2d03839dc7d504905a75fdf1be09c8
Nick Lee <leexnick@gmail.com>
2019-05-08 23:22:39 -0700

    update test/setup & test/app.spec

--- test/app.spec.js
+++ test/app.spec.js
@@ -1,9 +1,11 @@
 const app = require('../src/app')
+const { API_TOKEN } = require('../src/config')
 
 describe('App', () => {
   it('GET / responds with 200 containing "Hello, world!"', () => {
     return supertest(app)
       .get('/')
-      .expect(200, 'Hello, world!')
+      .set('Authorization', 'Bearer ' + API_TOKEN)
+      .expect(200, `Nick's Bookmarks Server!`);
   })
 })
